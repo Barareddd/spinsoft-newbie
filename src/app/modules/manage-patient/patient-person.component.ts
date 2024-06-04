d5b6f994ae587e64bd8211a0dfd32a69f8899bdb
Barared <best.siraboonchai@gmail.com>
2024-06-04 23:11:16 +0700

    add: Angula form

--- src/app/modules/manage-patient/patient-person.component.ts
+++ src/app/modules/manage-patient/patient-person.component.ts
@@ -1,37 +1,46 @@
-import { Component } from '@angular/core';
-import { LocalStorageService } from 'src/app/local-storage.service';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { LocalStorageService } from "src/app/local-storage.service";
 
 @Component({
-  selector: 'app-patient',
-  templateUrl: './patient-person.component.html',
-  styleUrls: ['./patient-person.component.scss']
+  selector: "app-patient",
+  templateUrl: "./patient-person.component.html",
+  styleUrls: ["./patient-person.component.scss"],
 })
 export class PatientPersonComponent {
   userData: any = {};
+  myForm: FormGroup;
 
-  constructor(private localStorageService: LocalStorageService) {}
-
-  saveToLocalStorage() {
-    const key = this.userData.name; // Use the name field as the key
-    if (!key) {
-      alert("Please enter a name before saving.");
-      return;
-    }
-    this.localStorageService.setItem(key, JSON.stringify(this.userData));
-    alert("Data saved to local storage!");
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      name: "",
+      surname: "",
+      number: "",
+    });
+    this.myForm.valueChanges.subscribe(console.log);
   }
 
-  retrieveFromLocalStorage() {
-    const key = this.userData.name; // Use the name field to retrieve data
-    if (!key) {
-      alert("Please enter a name to retrieve data.");
-      return;
-    }
-    const userDataString = this.localStorageService.getItem(key);
-    if (userDataString) {
-      this.userData = JSON.parse(userDataString);
-    } else {
-      console.log("No data found in local storage for the provided name!");
-    }
-  }
+  // saveToLocalStorage() {
+  //   const key = this.userData.name; // Use the name field as the key
+  //   if (!key) {
+  //     alert("Please enter a name before saving.");
+  //     return;
+  //   }
+  //   this.localStorageService.setItem(key, JSON.stringify(this.userData));
+  //   alert("Data saved to local storage!");
+  // }
+
+  // retrieveFromLocalStorage() {
+  //   const key = this.userData.name; // Use the name field to retrieve data
+  //   if (!key) {
+  //     alert("Please enter a name to retrieve data.");
+  //     return;
+  //   }
+  //   const userDataString = this.localStorageService.getItem(key);
+  //   if (userDataString) {
+  //     this.userData = JSON.parse(userDataString);
+  //   } else {
+  //     console.log("No data found in local storage for the provided name!");
+  //   }
+  // }
 }
