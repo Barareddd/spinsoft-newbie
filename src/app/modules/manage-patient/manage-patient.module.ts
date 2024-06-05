c76a9c107549fc9a43a402c016bee9821d6c86bd
Barareddd <best.siraboonchai@gmail.com>
2024-06-05 11:48:47 +0700

    mission 1: save to localstorage and retrive from that to show data

--- src/app/modules/manage-patient/manage-patient.module.ts
+++ src/app/modules/manage-patient/manage-patient.module.ts
@@ -1,29 +1,12 @@
 import { NgModule, LOCALE_ID } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ManagePatientRouting } from "./manage-patient.routing.module";
-import { WidgetsModule } from "../../_metronic/partials";
-import { InlineSVGModule } from "ng-inline-svg-2";
-import { AutocompleteLibModule } from "angular-ng-autocomplete";
+
 import { ReactiveFormsModule } from "@angular/forms";
 import { FormsModule } from "@angular/forms";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatInputModule } from "@angular/material/input";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatAutocompleteModule } from "@angular/material/autocomplete";
-import {
-  MatNativeDateModule,
-  DateAdapter,
-  MAT_DATE_FORMATS,
-} from "@angular/material/core";
-import {
-  AppDateAdapter,
-  APP_DATE_FORMATS,
-} from "../../services/helper/customDatePicker";
-import { MatSelectModule } from "@angular/material/select";
-import { MatIconModule } from "@angular/material/icon";
-import { MatButtonModule } from "@angular/material/button";
+
 // import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap/tooltip/tooltip.module'
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
 import localeSimplifiedThai from "@angular/common/locales/th";
 import { registerLocaleData } from "@angular/common";
 
--- src/app/modules/manage-patient/patient-person.component.ts
+++ src/app/modules/manage-patient/patient-person.component.ts
@@ -1,46 +1,86 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup } from "@angular/forms";
 import { LocalStorageService } from "src/app/local-storage.service";
+
+interface Patient {
+  name: string;
+  surname: string;
+  number: string;
+}
 
 @Component({
   selector: "app-patient",
   templateUrl: "./patient-person.component.html",
   styleUrls: ["./patient-person.component.scss"],
 })
-export class PatientPersonComponent {
-  userData: any = {};
+export class PatientPersonComponent implements OnInit {
   myForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private localStorageService: LocalStorageService
+  ) {
     this.myForm = this.fb.group({
       name: "",
       surname: "",
       number: "",
+      patients: [[] as Patient[]],
     });
+
     this.myForm.valueChanges.subscribe(console.log);
   }
 
-  // saveToLocalStorage() {
-  //   const key = this.userData.name; // Use the name field as the key
-  //   if (!key) {
-  //     alert("Please enter a name before saving.");
-  //     return;
-  //   }
-  //   this.localStorageService.setItem(key, JSON.stringify(this.userData));
-  //   alert("Data saved to local storage!");
-  // }
+  saveToLocalStorage() {
+    const formData = this.myForm.value;
 
-  // retrieveFromLocalStorage() {
-  //   const key = this.userData.name; // Use the name field to retrieve data
-  //   if (!key) {
-  //     alert("Please enter a name to retrieve data.");
-  //     return;
-  //   }
-  //   const userDataString = this.localStorageService.getItem(key);
-  //   if (userDataString) {
-  //     this.userData = JSON.parse(userDataString);
-  //   } else {
-  //     console.log("No data found in local storage for the provided name!");
-  //   }
-  // }
+    if (!formData.name || !formData.surname || !formData.number) {
+      alert("Please enter all required fields.");
+      return;
+    }
+
+    const patientData: Patient = {
+      name: formData.name,
+      surname: formData.surname,
+      number: formData.number,
+    };
+
+    const updatedPatients = [...formData.patients, patientData];
+
+    this.localStorageService.setItem(
+      "patientData",
+      JSON.stringify(updatedPatients)
+    );
+
+    alert("Data saved to local storage!");
+
+    this.myForm.patchValue({
+      patients: updatedPatients,
+      name: "",
+      surname: "",
+      number: "",
+    });
+
+    this.retrieveFromLocalStorage();
+  }
+
+  retrieveFromLocalStorage() {
+    const storedDataString = this.localStorageService.getItem("patientData");
+
+    if (storedDataString) {
+      const storedData: Patient[] = JSON.parse(storedDataString);
+
+      this.myForm.patchValue({
+        patients: storedData || [],
+      });
+
+      console.log(this.myForm.value.patients);
+      alert("Data retrieved from local storage!");
+    } else {
+      console.log("No data found in local storage!");
+    }
+  }
+
+  ngOnInit() {
+    this.retrieveFromLocalStorage();
+  }
 }
